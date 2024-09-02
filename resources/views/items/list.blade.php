88938b4ad56915874606ee5ecaa07aa096e557c8
Luna217-ops <fernando.garay.serpa@outlook.com>
2024-09-01 20:41:18 -0500

    Laboratorio 1: Routing

---
+++ resources/views/items/list.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de articulos</title>
+</head>
+<body>
+    <h1>Articulos</h1>
+
+    <ol>
+        <li>Item 01</li>
+        <li>Item 02</li>
+        <li>Item 03</li>
+        <li>Item 04</li>
+        <li>Item 05</li>
+        <li>Item 06</li>
+        <li>Item 07</li>
+        <li>Item 08</li>
+        <li>Item 09</li>
+        <li>Item 10</li>
+        <li>Item 11</li>
+    </ol>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,48 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*
+ Laboratorio 1
+ */
+
+// Crea una ruta en Laravel que responda a la URL /hello y devuelva el texto “¡Hola,Laravel!” cuando se acceda a ella.
+Route::get("/hello", function () {
+    return "¡Hola, Laravel!";
+});
+
+// Define una ruta que acepte un parámetro id y devuelva un mensaje que diga “El ID del artículo es [id]”, donde [id] es el valor del parámetro.
+Route::get("/item/{input_id}", function ($input_id) {
+    return "El ID del artículo es $input_id";
+});
+
+// Crea una ruta que acepte un parámetro opcional name. Si se proporciona, devuelve “Hola, [name]”; si no se proporciona, devuelve “Hola, invitado”.
+Route::get("/call-me/{who?}", function ($who = "invitado") {
+    return "Hola, $who";
+});
+
+
+// Define una ruta que acepte solo números en el parámetro orderId. Asegúrate de que la ruta solo se pueda acceder si orderId es un número entero.
+Route::get("/only-number/{orderId}", function ($orderId) {
+    return "Soy un entero $orderId";
+})->whereNumber('orderId', '[0-9]+');
+
+// Asigna el nombre profile a una ruta que acepte un parámetro username y muestra una vista que salude al usuario con su nombre. Usa el nombre de la ruta para generar la URL en una vista.
+Route::get("/profile/{username}", function ($username) {
+    return view('profile', ['username' => $username]);
+})->name('profile');
+
+// Crea una ruta /welcome que devuelva la vista welcome.blade.php. Asegúrate de que la vista se muestre cuando se accede a esta ruta.
+Route::get("/welcome", function () {
     return view('welcome');
 });
+
+// Crea una ruta /old-page que redirija a /new-page. Asegúrate de que la redirección funcione correctamente cuando accedas a /old-page.
+Route::redirect("/old-page", '/new-page');
+Route::get("/new-page", function () {
+    return view('new-page');
+});
+
+// Define una ruta /items que llame al método list del ItemController. Asegúrate de que esta ruta esté correctamente vinculada al método del controlador.
+Route::get("/items", [ItemController::class, 'list']);
